f8409af1e129c1f317ccb1d16d6bb9ecb15c9b20
Eemi Haukkala <eemi.haukkala@gmail.com>
2015-01-19 18:49:18 +0200

    Indent python test file correctly

--- functional-tests/base_tests.py
+++ functional-tests/base_tests.py
@@ -1,82 +1,78 @@
-from uiautomator import device as d
-import unittest
-from state_machine_crawler import State, Transition, StateMachineCrawler
-import commands
-from subprocess import Popen, PIPE, STDOUT
 import sys
+import unittest
+
+from subprocess import Popen, PIPE, STDOUT
+from state_machine_crawler import State, Transition, StateMachineCrawler
+from uiautomator import device as d
+
 
 class CommandExecutionError(Exception):
-   pass
+    pass
+
 
 def call(*command):
-   process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
+    process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
 
-   outputs = []
+    outputs = []
 
-   # Poll process for new output until finished
-   while True:
-       nextline = process.stdout.readline()
-       if nextline == '' and process.poll() is not None:
-           break
-       sys.stdout.write(nextline)
-       outputs.append(nextline)
-       sys.stdout.flush()
+    # Poll process for new output until finished
+    while True:
+        nextline = process.stdout.readline()
+        if nextline == '' and process.poll() is not None:
+            break
+        sys.stdout.write(nextline)
 
-   output = "\n".join(outputs)
-   exitCode = process.returncode
+    outputs.append(nextline)
+    sys.stdout.flush()
 
-   if exitCode == 0:
-       return output
-   else:
-       raise CommandExecutionError(command, exitCode, output)
+    output = "\n".join(outputs)
+    exitCode = process.returncode
+
+    if exitCode == 0:
+        return output
+    else:
+        raise CommandExecutionError(command, exitCode, output)
 
 
 class InstalledState(State):
 
-	def verify(self):
-		cmd = "adb shell 'pm list packages -f | grep com.fsecure.lokki'"
-		return call(cmd)
+    def verify(self):
+        cmd = "adb shell 'pm list packages -f | grep com.fsecure.lokki'"
+        return call(cmd)
+
 
 class InitialTransition(Transition):
-	target_state = InstalledState
+    target_state = InstalledState
 
-	def move(self):
-              	cmd = "adb install -r App/build/outputs/apk/lokki-*-debug.apk"
-		call(cmd)
+    def move(self):
+        cmd = "adb install -r App/build/outputs/apk/lokki-*-debug.apk"
+        call(cmd)
+
 
 class LokkiMainActivityState(State):
 
-	def verify(self):
-		abc = d(text='Lokki', packageName='com.fsecure.lokki').wait.exists(timeout = 10000)
-		print "LOLOLOLOLOL: " + str(abc)
-		return abc
+    def verify(self):
+        abc = d(text='Lokki', packageName='com.fsecure.lokki').wait.exists(timeout=10000)
+        print "LOLOLOLOLOL: " + str(abc)
+        return abc
 
-	class LaunchLokkiTransition(Transition):
-		source_state = InstalledState
+    class LaunchLokkiTransition(Transition):
+        source_state = InstalledState
 
-		def move(self):
-			cmd = "adb shell am start -n com.fsecure.lokki/com.fsecure.lokki.MainActivity"
-			call(cmd)
-	
+        def move(self):
+            cmd = "adb shell am start -n com.fsecure.lokki/com.fsecure.lokki.MainActivity"
+            call(cmd)
+
 
 class BaseTest(unittest.TestCase):
 
-	def setUp(self):
-		self.cr = StateMachineCrawler(d, InitialTransition)
+    def setUp(self):
+        self.cr = StateMachineCrawler(d, InitialTransition)
 
+    def test_lokki_install(self):
+        self.cr.move(InstalledState)
+        self.assertIs(self.cr.state, InstalledState)
 
-	def test_lokki_install(self):
-		self.cr.move(InstalledState)
-		self.assertIs(self.cr.state, InstalledState)
-
-	def test_lokki_is_launched(self):
-		self.cr.move(LokkiMainActivityState)
-		self.assertIs(self.cr.state, LokkiMainActivityState)
-		
-		
-
-
-
-
-
-
+    def test_lokki_is_launched(self):
+        self.cr.move(LokkiMainActivityState)
+        self.assertIs(self.cr.state, LokkiMainActivityState)
