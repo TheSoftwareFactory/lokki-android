04e5ccc5a4010cd16c3cbc247aab7d22d6b67ebd
Eemi Haukkala <eemi.haukkala@gmail.com>
2015-01-26 16:46:28 +0200

    Added contact adding tests

--- App/src/test/java/com/fsecure/lokki/espresso/AddContactsScreenTest.java
+++ App/src/test/java/com/fsecure/lokki/espresso/AddContactsScreenTest.java
@@ -1,10 +1,7 @@
 package com.fsecure.lokki.espresso;
 
 import android.content.Context;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.InstrumentationTestCase;
 
-import com.fsecure.lokki.MainActivity;
 import com.fsecure.lokki.R;
 import com.fsecure.lokki.utils.ContactUtils;
 
@@ -12,15 +9,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.core.AnyOf.anyOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
@@ -55,8 +55,16 @@ public class AddContactsScreenTest extends MainActivityBaseTest {
     }
 
     private void addSingleContactFromContactListScreen(String contactName) {
+        ArrayList<String> contactArray = new ArrayList<>();
+        contactArray.add(contactName);
+        addMultipleContactsFromContactListScreen(contactArray);
+    }
+
+    private void addMultipleContactsFromContactListScreen(List<String> contactsArray) {
         onView(withId(R.id.add_people)).perform(click());
-        onView(allOf(withId(R.id.contact_selected), withText(contactName))).perform(click());
+        for (String contactName : contactsArray) {
+            onView(allOf(withId(R.id.contact_selected), hasSibling(withText(contactName)))).perform(click());
+        }
         onView(withId(R.id.allow_people)).perform(click());
     }
 
@@ -83,17 +91,55 @@ public class AddContactsScreenTest extends MainActivityBaseTest {
 
     public void testAddingSingleContactShowsDialog() {
         String contactName = "Family Member";
+        String contactAddedDialogText = TestUtils.getStringFromResources(getInstrumentation(), R.string.add_contact_dialog_save);
+
         addSingleContactFromContactListScreen(contactName);
-        onView(allOf(withText(contactName), withText(R.string.add_contact_dialog_save))).check(matches(isDisplayed()));
+
+        onView(withText(containsString(contactName))).check(matches(isDisplayed()));
+        onView(withText(containsString(contactAddedDialogText))).check(matches(isDisplayed()));
     }
 
     public void testAddingSingleContactAddsContactToList() {
         String contactName = "Family Member";
+
         addSingleContactFromContactListScreen(contactName);
-        onView(withText("Ok")).perform(click());
+        onView(withText("OK")).perform(click());
         enterContactsScreen();
+
         onView(withText(contactName)).check(matches(isDisplayed()));
     }
+
+    public void testAddingTwoContactsShowsDialog() {
+        String firstContactName = "Family Member";
+        String secondContactName = "Test Friend";
+        ArrayList<String> contactList = new ArrayList<>();
+        contactList.add(firstContactName);
+        contactList.add(secondContactName);
+        String contactAddedDialogText = TestUtils.getStringFromResources(getInstrumentation(), R.string.add_contact_dialog_save);
+
+        addMultipleContactsFromContactListScreen(contactList);
+
+        onView(withText(containsString(firstContactName))).check(matches(isDisplayed()));
+        onView(withText(containsString(secondContactName))).check(matches(isDisplayed()));
+        onView(withText(containsString(contactAddedDialogText))).check(matches(isDisplayed()));
+    }
+
+    public void testAddTwoContactsAddsContactToList() {
+        String firstContactName = "Family Member";
+        String secondContactName = "Test Friend";
+        ArrayList<String> contactList = new ArrayList<>();
+        contactList.add(firstContactName);
+        contactList.add(secondContactName);
+
+        addMultipleContactsFromContactListScreen(contactList);
+        onView(withText("OK")).perform(click());
+        enterContactsScreen();
+
+        onView(withText(firstContactName)).check(matches(isDisplayed()));
+        onView(withText(secondContactName)).check(matches(isDisplayed()));
+    }
+
+    
 
 
 }
