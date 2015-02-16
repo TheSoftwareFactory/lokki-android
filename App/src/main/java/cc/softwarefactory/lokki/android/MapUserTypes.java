48b5747cef3093b849e43a7bdce99c31086a5c4e
vforteli <verner@fortelius.net>
2015-02-16 11:12:31 +0200

    Get rid of some magic numbers

---
+++ App/src/main/java/cc/softwarefactory/lokki/android/MapUserTypes.java
@@ -0,0 +1,17 @@
+package cc.softwarefactory.lokki.android;
+
+/**
+ * Created by verne_000 on 16.2.2015.
+ */
+public enum MapUserTypes {
+    User(0),
+    Others(1),
+    All(2);
+    // 0 = user, 1 = others, 3 = all.   <- huh? in the code 2 is used for all
+
+    private int value;
+
+    private MapUserTypes(int value) {
+        this.value = value;
+    }
+}
