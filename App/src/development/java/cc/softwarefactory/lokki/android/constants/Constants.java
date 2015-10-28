ed680900c6e3f425eb2a3cc7db8e9a45b55b75e2
Sandeep <sandeep.panchamukhi@gmail.com>
2015-10-28 12:14:27 +0200

    test commit

--- App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
+++ App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
@@ -1,5 +1,6 @@
 package cc.softwarefactory.lokki.android.constants;
 
 public class Constants {
+    //define API end Point
     public static final String API_URL = "https://lokki-dev.herokuapp.com/api/locmap/v1/";
 }
