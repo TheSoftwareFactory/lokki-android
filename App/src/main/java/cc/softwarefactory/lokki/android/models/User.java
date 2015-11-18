488e964daf29652345f91a7660da9c4ceb6a7c5a
Sandeep <sandeep.panchamukhi@gmail.com>
2015-11-18 15:45:36 +0200

    Added gettter and setter for model class

--- App/src/main/java/cc/softwarefactory/lokki/android/models/User.java
+++ App/src/main/java/cc/softwarefactory/lokki/android/models/User.java
@@ -1,64 +1,12 @@
 package cc.softwarefactory.lokki.android.models;
 
 
-import java.util.Date;
-
 public class User extends JSONModel {
-    public static class Location extends JSONModel {
-        private double lat;
-        private double lon;
-        private double acc;
-        private Date time;
 
-        public android.location.Location convertToAndroidLocation() {
-            android.location.Location convertedLocation = new android.location.Location("fused");
-            double lat = this.lat;
-            double lon = this.lon;
-            float acc = (float) this.acc;
-            Long time = (this.time == null) ? 0 : this.time.getTime();
-            convertedLocation.setLatitude(lat);
-            convertedLocation.setLongitude(lon);
-            convertedLocation.setAccuracy(acc);
-            convertedLocation.setTime(time);
-            return convertedLocation;
-        }
 
-        public double getLat() {
-            return lat;
-        }
-
-        public void setLat(double lat) {
-            this.lat = lat;
-        }
-
-        public double getLon() {
-            return lon;
-        }
-
-        public void setLon(double lon) {
-            this.lon = lon;
-        }
-
-        public double getAcc() {
-            return acc;
-        }
-
-        public void setAcc(double acc) {
-            this.acc = acc;
-        }
-
-        public Date getTime() {
-            return time;
-        }
-
-        public void setTime(Date time) {
-            this.time = time;
-        }
-    }
-
-    private String battery;
-    private Location location;
+    private UserLocation location;
     private boolean visibility;
+    private String battery;
 
     public String getBattery() {
         return battery;
@@ -68,11 +16,11 @@ public class User extends JSONModel {
         this.battery = battery;
     }
 
-    public Location getLocation() {
+    public UserLocation getUserLocation() {
         return location;
     }
 
-    public void setLocation(Location location) {
+    public void setUserLocation(UserLocation location) {
         this.location = location;
     }
 
@@ -82,5 +30,13 @@ public class User extends JSONModel {
 
     public void setVisibility(boolean visibility) {
         this.visibility = visibility;
+    }
+
+    public UserLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(UserLocation location) {
+        this.location = location;
     }
 }
