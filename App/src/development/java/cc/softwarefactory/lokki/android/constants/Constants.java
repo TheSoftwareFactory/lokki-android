193e8bf6dc12df15755bfefac4804aa9afefd3e7
Olli Raitio <olli.raitio@helsinki.fi>
2015-11-27 15:00:33 +0200

    forgot to toggle local test server address off

--- App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
+++ App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
@@ -2,6 +2,5 @@ package cc.softwarefactory.lokki.android.constants;
 
 public class Constants {
     //define API end Point
-//    public static final String API_URL = "https://lokki-dev.herokuapp.com/api/locmap/v2/";
-    public static final String API_URL = "http://10.0.2.2:9000/api/locmap/v2/";
+    public static final String API_URL = "https://lokki-dev.herokuapp.com/api/locmap/v2/";
 }
