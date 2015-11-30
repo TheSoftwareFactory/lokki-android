ea0ceaebf7de01b3ee8b1dd0f054ae191aa6b7b7
suleymanakbas91 <suleymanakbas91@gmail.com>
2015-11-30 13:38:18 +0200

    Revert changed name

--- App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
+++ App/src/development/java/cc/softwarefactory/lokki/android/constants/Constants.java
@@ -3,5 +3,4 @@ package cc.softwarefactory.lokki.android.constants;
 public class Constants {
     //define API end Point
     public static final String API_URL = "https://lokki-dev.herokuapp.com/api/locmap/v2/";
-    
 }
