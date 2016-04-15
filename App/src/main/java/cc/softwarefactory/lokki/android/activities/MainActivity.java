7ab306d46d8b15bd01e6436c93f1f30f6de595a4
26can <nicolefortunecat@msn.cn>
2016-04-15 13:28:17 +0300

    Hide Soft Keyboard #479

--- App/src/main/java/cc/softwarefactory/lokki/android/activities/MainActivity.java
+++ App/src/main/java/cc/softwarefactory/lokki/android/activities/MainActivity.java
@@ -29,6 +29,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
@@ -430,6 +431,11 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerF
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        View view = this.getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
         return true;
     }
 
